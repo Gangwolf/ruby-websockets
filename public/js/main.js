6bcb48376f62e5fe1433e5048f505a70f2e8c07a
Dan <gangwolf@programmer.net>
2016-04-22 00:04:57 +0000

    Escape HTML and fix some bugs

--- public/js/main.js
+++ public/js/main.js
@@ -1,25 +1,48 @@
 var ws = new WebSocket('ws://' + window.document.location.host);
-
-var name = prompt("Enter name");
-
-ws.onmessage = function(message) {
-  var data = JSON.parse(message.data);
-  /*With an open WebSocket, the browser will receive messages. These messages are structured as a JSON response with two keys: name (name’s name) and text (name’s message). When the message is received, it’s inserted as a new entry in the page.*/
-  var al = '';
-  var resText = '';
-  if (data.name === name) {
-  	al = 'u-pull-left';
-  } else {
-  	al = 'u-pull-right';
-  	resText = ' resText';
-  }
-  $('#chat').append("<div class='row'><div class='" + al + resText +" response'><strong>" + data.name + "</strong id='resName'><p>" + data.text + '</p></div></div>');
+var uid = parseInt((Math.random() * 9 + 1) * Math.pow(10, 4 - 1), 10);
+var name = prompt("Enter name", "Eobard Thawne");
+var entityMap = {
+    "&": "&amp;",
+    "<": "&lt;",
+    ">": "&gt;",
+    '"': '&quot;',
+    "'": '&#39;',
+    "/": '&#x2F;'
 };
 
+function escapeHtml(string) {
+    // Thank you mustache.js!
+    return String(string).replace(/[&<>"'\/]/g, function(s) {
+        return entityMap[s];
+    });
+}
+
+function removeUid(string) {
+    return string.substring(0, string.length - 4);
+}
+ws.onmessage = function(message) {
+    var data = JSON.parse(message.data);
+    var al = '';
+    var resText = '';
+    if (data.name === name + uid) {
+        al = 'u-pull-left';
+    } else {
+        al = 'u-pull-right';
+        resText = ' resText';
+    }
+    $('#chat').append("<div class='row'><div class='" + al + resText + " response'><strong>"
+    + escapeHtml(removeUid(data.name)) + "</strong id='resName'><p>" + escapeHtml(data.text) + '</p></div></div>');
+};
 $('form').on('submit', function(event) {
-  /* override the form submit button to grab the the values from the form and send them as a JSON message over the WebSocket to the server.*/	
-  event.preventDefault();
-  var text = $('#message')[0].value;
-  ws.send(JSON.stringify({ name: name, text: text }));
-  $('#message')[0].value = '';
+    event.preventDefault();
+    var text = $('#message')[0].value;
+    if ($.trim(text)) {
+        ws.send(JSON.stringify({
+            name: name + uid,
+            text: text
+        }));
+    } else {
+        alert('Write a messege');
+    }
+    text = '';
 });
