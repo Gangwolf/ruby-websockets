38ced72182902a98da360a5699e03908f074afb6
Dan <gangwolf@programmer.net>
2016-04-22 00:06:54 +0000

    Clean up code

--- services/wschat/server.rb
+++ services/wschat/server.rb
@@ -1,7 +1,4 @@
 require 'faye/websocket'
-#require "cgi"
-#require 'rack/utils'
-
 Faye::WebSocket.load_adapter('thin')
 
 module Web
@@ -18,27 +15,22 @@ module Web
     	if Faye::WebSocket.websocket?(env)
     		ws = Faye::WebSocket.new(env, nil, { max_length: MAX_LENGTH, ping: PING_FREQUENCY })
     		ws.on :open do |event|
-    			#p :open
     			@users << ws
     		end
 
     		ws.on :message do |event|
-    			#p [:message, event.data]
     			@users.each do |user|
-                    #CGI::escape_element(event.data)
-                    #Rack::Utils.escape_html(event.data)
     				user.send(event.data)
     			end
     		end
 
-            ws.on :error do |event|
-                @users.each do |user|
-                    user.send("Bad data sent by peer")
-                end
-            end 
+        ws.on :error do |event|
+            @users.each do |user|
+                user.send("Bad data sent by peer")
+            end
+        end 
 
     		ws.on :close do |event|
-    			#p [:close, event.code, event.reason]
     			@users.delete(ws)
     			ws = nil
     		end
